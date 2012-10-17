1ca4072408b2ef40910dab5569b18d73440f047b
flyingman <flyingman26@sina.com>
2012-10-17 17:36:17 +0800

    hanoi.c

---
+++ hanoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+FLYINGMAN
+hanoi problem, recursion method
+ */
+/* move the plates number to pillar */
+void move(int number, char from, char to){
+  printf("%d, %c --> %c\n", number, from, to);
+}
+
+/* move n plates from a to c through b */
+void hanoi(char a, char b, char c, int n){
+  if(n == 1){
+    move(1, a, c);
+  } else{
+    hanoi(a, c, b, n-1);
+    move(n, a, c);
+    hanoi(b, a, c, n-1);
+  }
+}
+
+void main(){
+  int n;
+  printf("input the number of plates: \n");
+  scanf("%d", &n);
+  char a = 'a', b = 'b', c = 'c';
+  hanoi(a, b, c, n);
+}
